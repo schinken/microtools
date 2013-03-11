8005d160c2028a9626c858dbdc3c7409af41d1ee
schinken <schinken@hackerspace-bamberg.de>
2013-03-11 19:23:36 +0100

    removed external file

--- mac_vendors/mac_vendors.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS mac_vendors (
-    id INT(11) auto_increment PRIMARY KEY,
-    macaddr CHAR(8),
-    vendor VARCHAR(64),
-    PRIMARY KEY  (id),
-    KEY macaddr (macaddr)
-) ENGINE=MyISAM;
-
