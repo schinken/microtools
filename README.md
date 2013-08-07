b801444d8001999f3af9aea6d8c4d8a7fd88f24b
schinken <schinken@hackerspace-bamberg.de>
2013-08-07 14:01:43 +0200

    added account balance reader

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ just a simple "collection" of microtools i have written to process simple tasks
 ## krpano_px2deg.py
 
 A simple tool to convert a pixel coordinate to a degree-coordinate for krpano hotspots needed in tour.xml
+
+## aqbanking
+
+Tiny script used for backspace to retrieve the account balance with an cronjob, and write it to the database and into a file for snmp query
---
+++ aqbanking/get_balance.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+#############################################################################
+# Configuration
+
+# Database Credentials
+SQL_HOST="violet"
+SQL_USER="bank"
+SQL_PASS="lollolol"
+SQL_NAME="mydb"
+
+# Files
+AQBANKING_CLI="/usr/bin/aqbanking-cli"
+PINFILE="/tmp/pinfile"
+CTX_FILE="/tmp/tmp.ctx"
+SNMP_FILE="/tmp/account_balance.snmp"
+
+# HBCI Credentials
+USER=""
+PIN=""
+
+# Account credentials
+BLZ=""
+KTO=""
+
+#############################################################################
+# HBCI Magic
+
+# Create temporary pinfile
+echo "PIN_${BLZ}_${USER} = \"$PIN\"" > $PINFILE
+
+# request account balance from bank
+$AQBANKING_CLI -n -P $PINFILE request -b $BLZ -a $KTO -c $CTX_FILE --balance 2> /dev/null
+
+# retrieve balance from CTX file
+BALANCE=`$AQBANKING_CLI listbal -b $BLZ -a $KTO -c $CTX_FILE 2> /dev/null | awk '{print $10}'`
+
+# Context-File, Pinfile is not needed anymore
+rm -f $CTX_FILE
+rm -f $PINFILE
+
+echo $BALANCE
+
+# check if number is numeric
+if echo $BALANCE | egrep -v -q '^[0-9]+\.[0-9]+$'; then
+    echo "$BALANCE is not a number"
+    exit 1
+fi
+
+#############################################################################
+# Store dat shit
+
+echo $BALANCE > $SNMP_FILE
+
+MYSQL_PWD="$SQL_PASS"
+echo "INSERT INTO account (blz, kto, balance, erfda) VALUE ('$BLZ', '$KTO', $BALANCE, NOW())" | \
+        mysql -h "$SQL_HOST" -u "$SQL_USER" "$SQL_NAME"
+
+unset MYSQL_PWD
+
+exit 0;
